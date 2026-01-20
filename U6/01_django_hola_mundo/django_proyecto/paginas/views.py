6e6a97ba200bbacc5beacc6861a65ed3866a7b50
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2026-01-20 11:54:42 +0100

    Terminada práctica 01 Hola Mundo Django con gitignore

--- U6/01_django_hola_mundo/django_proyecto/paginas/views.py
+++ U6/01_django_hola_mundo/django_proyecto/paginas/views.py
@@ -1,3 +1,6 @@
+# pages/views.py
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def vista_pagina_inicio(request):
+    return HttpResponse("Hola mundo, me llamo Álvaro!")
