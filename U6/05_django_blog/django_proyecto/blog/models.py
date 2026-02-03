28ba09e8f309182d1cc2287afaa7b4414e777066
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2026-02-03 09:20:09 +0100

    Termino P5 Django

--- U6/05_django_blog/django_proyecto/blog/models.py
+++ U6/05_django_blog/django_proyecto/blog/models.py
@@ -1,13 +1,20 @@
 # blog/models.py
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     titulo = models.CharField(max_length=200)
-    autor = models.ForeignKey("auth.User",
-                            on_delete=models.CASCADE,
-                            ) # nuevo
+
+    autor = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+    ) # nuevo
+
     cuerpo = models.TextField()
-    
+
     def __str__(self):
         return self.titulo
+    
+    def get_absolute_url(self): # nuevo
+        return reverse("detalle_post", kwargs={"pk": self.pk})
     
